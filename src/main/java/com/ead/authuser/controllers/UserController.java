d42a1f5577dd3edfa445b89b2d155d363f135180
Yane Wanderley <yane.rodrigues@ufpe.br>
2024-04-29 21:17:05 -0300

    Authorization com Roles em Microservices

--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -1,5 +1,8 @@
 package com.ead.authuser.controllers;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Optional;
@@ -12,6 +15,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,18 +27,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ead.authuser.configs.security.AuthenticationCurrentUserService;
+import com.ead.authuser.configs.security.UserDetailsImpl;
 import com.ead.authuser.dtos.UserDto;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.ead.authuser.specifications.SpecificationTemplate;
 import com.fasterxml.jackson.annotation.JsonView;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/users")
@@ -39,10 +47,19 @@ public class UserController {
 
     @Autowired
     UserService userService;
-
+    
+    @Autowired
+    AuthenticationCurrentUserService authenticationCurrentUserService;
+    
+    
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping
     public ResponseEntity<Page<UserModel>> getAllUSers(SpecificationTemplate.UserSpec spec,
-    		@PageableDefault(page = 0, size = 1, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable){
+    		@PageableDefault(page = 0, size = 1, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable,
+    		Authentication authentication){
+    	UserDetails userDetails = (UserDetailsImpl) authentication.getPrincipal();
+    	log.info("Authentication {}", userDetails.getUsername());
+    	
     	Page<UserModel> userModelPage = userService.findAll(spec, pageable);
     			
     	if(!userModelPage.isEmpty()) {
@@ -55,14 +72,22 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(userModelPage);
     }
 
+    @PreAuthorize("hasAnyRole('STUDENT')")
 	@GetMapping("/{userId}")
     public ResponseEntity<Object> getOneUser(@PathVariable(value = "userId") UUID userId){
-        Optional<UserModel> userModelOptional = userService.findById(userId);
-        if(!userModelOptional.isPresent()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("UsuÃ¡rio nÃ£o encontrado!");
-        }else{
-            return ResponseEntity.status(HttpStatus.OK).body(userModelOptional.get());
-        }
+    	UUID currentUserId = authenticationCurrentUserService.getCurrentUser().getUserId();
+    	
+    	if(currentUserId.equals(userId)) {
+    		Optional<UserModel> userModelOptional = userService.findById(userId);
+            if(!userModelOptional.isPresent()){
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("UsuÃ¡rio nÃ£o encontrado!");
+            }else{
+                return ResponseEntity.status(HttpStatus.OK).body(userModelOptional.get());
+            }	
+    	}else {
+    		throw new AccessDeniedException("Acesso proibido!");
+    	}
+        
     }
 
     @DeleteMapping("/{userId}")
