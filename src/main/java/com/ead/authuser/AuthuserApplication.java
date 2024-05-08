d0aba1c448b2fc61e4e0885daf95bcb6afecfa12
Yane Wanderley <yane.rodrigues@ufpe.br>
2024-05-08 02:05:39 -0300

    Criando branch pre producao com tudo funcionando

--- src/main/java/com/ead/authuser/AuthuserApplication.java
+++ src/main/java/com/ead/authuser/AuthuserApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class AuthuserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthuserApplication.class, args);
+		SpringApplication.run(AuthuserApplication.class, args); 
 	}
 
 }
